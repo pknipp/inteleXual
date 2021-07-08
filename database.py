e4c9b862e9f923d70b35d9c162d280a9c9f92ad7
Peter Knipp <peteraknipp@gmail.com>
2021-07-07 21:07:21 -0400

    Create users table.

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-from python_fs.models import User
-from python_fs import app, db
+from inteleXual.models import User
+from inteleXual import app, db
 from dotenv import load_dotenv
 from datetime import date, datetime, timedelta
 from faker import Faker
@@ -10,7 +10,7 @@ seed(1)
 fake = Faker()
 load_dotenv()
 
-users = [("demo@aol.com", "whatever"), ("jdoe@aol.com", "whatever")]
+users = [("demo@aol.com", "Demo User"), ("jdoe@aol.com", "John Doe")]
 
 with app.app_context():
     db.drop_all()
@@ -19,6 +19,7 @@ with app.app_context():
         created_at = fake.date_time_between(start_date=datetime(2000, 1, 15))
         db.session.add(User(
             email=user[0],
+            name=user[1],
             password="password",
             created_at=created_at,
             updated_at=fake.date_time_between(start_date=created_at)
